bcb917d675a15ec8a0d215f5b5874fc454877a74
FraterV <fraterv@online.de>
2012-06-15 10:46:07 +0200

    main.cpp angelegt

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: mmeese
+ *
+ * Created on 15. Juni 2012, 10:39
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
