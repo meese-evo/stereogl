9aa90f60b246b0bf631914880d7d935b44ee964a
FraterV <fraterv@online.de>
2012-06-15 15:44:49 +0200

    Git Infos hinzugefügt

--- main.cpp
+++ main.cpp
@@ -1,22 +1,36 @@
-#include <cstdlib>
-#include <cstdio>
-#include <iostream>
+#include "GL/freeglut.h"
+#include "GL/gl.h"
 
-using namespace std;
-
-/*
- * 
- */
-int main(int argc, char** argv) {
-    
-    int i;
-    char text[15]="Hello World!";
-    
-    for (i=0; i<=4; i++) {
-        printf("%i. printf: %s\n",i, text);
-        cout << i << ". cout: " << text << endl;
-    }
-
-    return 0;
+/* display function - code from:
+     http://fly.cc.fer.hr/~unreal/theredbook/chapter01.html
+This is the actual usage of the OpenGL library. 
+The following code is the same for any platform */
+void renderFunction()
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+    glBegin(GL_POLYGON);
+        glVertex2f(-0.5, -0.5);
+        glVertex2f(-0.5, 0.5);
+        glVertex2f(0.5, 0.5);
+        glVertex2f(0.5, -0.5);
+    glEnd();
+    glFlush();
 }
 
+/* Main method - main entry point of application
+the freeglut library does the window creation work for us, 
+regardless of the platform. */
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(500,500);
+    glutInitWindowPosition(100,100);
+    glutCreateWindow("OpenGL - First window demo");
+    glutDisplayFunc(renderFunction);
+    glutMainLoop();    
+    return 0;
+}
