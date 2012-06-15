d00e98d7c2836684ab30d74ccb7e3b7422f6bf31
FraterV <fraterv@online.de>
2012-06-15 11:39:49 +0200

    So... jetzt könnte es gehen...

--- main.cpp
+++ main.cpp
@@ -12,8 +12,7 @@ int main(int argc, char** argv) {
     int i;
     char text[15]="Hello World!";
     
-    for (i=0; i<=4; i++) 
-    {
+    for (i=0; i<=4; i++) {
         printf("%i. printf: %s\n",i, text);
         cout << i << ". cout: " << text << endl;
     }
