3500c9a2cf243fc29c2cab31a1d4151557bf7525
FraterV <fraterv@online.de>
2012-06-24 16:38:02 +0200

    SFML und OpenGL Testprojekte hinzugefügt: opengl_sfml_testsrc.zip

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 /*
- * main.cpp
- *
- *  Created on: 24.06.2012
- *      Author: mmeese
- */
+* Folgende Libs dem Linker hinzufügen
+* - sfml-system
+*
+*/
 
+#include <SFML/System.hpp>
+#include <iostream>
 
+using namespace std;
+using namespace sf;
 
+int main()
+{
+    Clock Clock;
+    while (Clock.GetElapsedTime() < 5.f)
+    {
+     cout << Clock.GetElapsedTime() << endl;
+        Sleep(0.5f);
+    }
 
+    return 0;
+}
