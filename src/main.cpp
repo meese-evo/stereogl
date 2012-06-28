5dda45dd9000dd7150adc03cda95358b19dd0677
master@volavis <pelennor@online.de>
2012-06-28 16:50:39 +0200

    Licht

--- src/main.cpp
+++ src/main.cpp
@@ -85,8 +85,8 @@ int main() {
 
 		glMatrixMode(GL_MODELVIEW);
 		glEnable(GL_COLOR_MATERIAL); //Materialfarbgebungsart
-		glEnable(GL_LIGHT1); //Lichtquelle 0 EIN
 		glEnable(GL_LIGHTING); //OpenGL-Lichteffekte EIN
+		glEnable(GL_LIGHT0); //Lichtquelle 0 EIN
 		glDisable(GL_CULL_FACE); //Seiten nicht ausblenden
 		glLoadIdentity();
 		glTranslatef(0.f, 0.f, -200.f);
